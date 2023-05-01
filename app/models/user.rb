b8932a0d7b3ef8d081ba4e68b2a1b7f4b9cca45a
EC2 Default User <ec2-user@ip-172-31-25-41.ap-northeast-1.compute.internal>
2023-05-01 07:22:25 +0000

    4

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,22 @@ class User < ApplicationRecord
   # 下記active_relationshipsはアソシエーションが繋がっているテーブル名である
   # class_nameは実際のモデルの名前:Relationship
   # foreign_keyは外部キーとして何を持つかを表しています。
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   #「has_many :テーブル名, through: :中間テーブル名」 の形を使って、テーブル同士が中間テーブルを通じてつながっていることを表現します。(followerテーブルとfollowedテーブルのつながりを表す）
   # 例えば、下記followersにfollowedを入れてしまうと、followedテーブルから中間テーブルを通ってfollowerテーブルにアクセスすることができなくなってしまいます。
   # これを防ぐために下記followersには架空のテーブル名を、zzzは実際にデータを取得しにいくテーブル名を書きます。
-  has_many :followers, through: :active_relationships, source: :followed
-
   #この結果、@user.上のfollowersとすることでそのユーザーがフォローしている人orフォローされている人の一覧を表示することができるようになります。
+
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
 
 
   has_one_attached :profile_image
@@ -29,5 +37,17 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+  
+  
   # いいね機能の場合は「1人のユーザーは1つの投稿に対して1回しかいいねできない」という仕様であるため、
   # destroyをする際にもユーザーidと投稿(book)idが分かれば、どのいいねを削除すればいいのかが特定できます。
   # そのため、いいねのidはURLに含める必要がない(params[:id]を使わなくても良い)ため、
